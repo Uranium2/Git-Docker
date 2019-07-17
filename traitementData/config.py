7fb1ee8aee996e4d7c9866b48a9e84df037c50c9
El-Tatane <antoine@boudeville.fr>
2019-07-17 19:30:07 +0200

    :wrench: add dynamic configuration

---
+++ traitementData/config.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+
+mongoServer = "mongodb://localhost:27017/"
+idMin = 12000
+idMax = 12200
+
+uriNeo = "http://localhost:7474"
+passwordNeo = "root"
+
+#Instance variables mongoDB
+# client = MongoClient('localhost', 27017)
+# database = client.myMovies
+# movies = database.movies
+# persons = database.persons
